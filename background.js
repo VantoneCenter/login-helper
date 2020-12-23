861ab50733a7fc59064a374ac706da0732d3333c
杨勇 <1994101600@qq.com>
2020-12-23 16:30:27 +0800

    feat: send to content when change switch

--- background.js
+++ background.js
@@ -8,9 +8,9 @@ chrome.runtime.onMessage.addListener(msg => {
         chrome.browserAction.setIcon({path: "/images/icon_gray.png"});
       }
     }
-    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    //   chrome.tabs.sendMessage(tabs[0].id, {...msg, to: 'content'});
-    // })
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+      chrome.tabs.sendMessage(tabs[0].id, {...msg, to: 'content'});
+    })
   }
 });
 
